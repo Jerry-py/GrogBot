4dfedd3d6465541cda9fafe2d103b1a80ad953e3
Jerry.py <Theofficialjerrypy@gmail.com>
2022-04-18 19:16:39 -0400

    Add trigonometry commands

--- core/cogs/math.py
+++ core/cogs/math.py
@@ -1,7 +1,7 @@
 import discord
+import math
 from discord.ext import commands
-from math import factorial
-
+from typing import Union
 try:
     from sympy.mpmath import mp
 except ImportError:
@@ -14,7 +14,7 @@ class Math(commands.Cog):
     @commands.command(help="Factorial of...")
     async def factorial(self, ctx, number=1):
         if number<=807:
-            result = factorial(number)
+            result = math.factorial(number)
             await ctx.send(result)
         else:
             embed = discord.Embed(title="Error",
@@ -42,6 +42,65 @@ class Math(commands.Cog):
                                   description="Must be 2000 or fewer in length",
                                   colour=0xe74c3c)
             await ctx.send(embed=embed)
+
+    @commands.group(help="Trigonometry\nFunctions : sin, sinh, asin, asinh, cos, cosh, acos, acosh, tan, tanh, atan, atanh\nNumbers in radians",
+                      aliases=["trig", "trigon", "trigonom"])
+    async def trigonometry(self, ctx, method=None, number=0):
+        mbed = discord.Embed(title="Trigonometry Commands",
+                            description="""
+                            Trigonometry\nFunctions : `sin`, `sinh`, `asin`, `asinh`, `cos`, `cosh`, `acos`, `acosh`, `tan`, `tanh`, `atan`, `atanh`\nNumbers in radians
+                            """
+                        )
+
+        await ctx.send(embed=mbed)
+    
+    @trigonometry.command()
+    async def sin(self, ctx, num : Union[int, float]):
+        return await ctx.send(math.sin(num))
+
+    @trigonometry.command()
+    async def sinh(self, ctx, num : Union[int, float]):
+        return await ctx.send(math.sinh(num))
+
+    @trigonometry.command()
+    async def asin(self, ctx, num : Union[int, float]):
+        return await ctx.send(math.asin(num))
+    
+    @trigonometry.command()
+    async def asinh(self, ctx, num : Union[int, float]):
+        return await ctx.send(math.asinh(num))
+    
+    @trigonometry.command()
+    async def cos(self, ctx, num : Union[int, float]):
+        return await ctx.send(math.cos(num))
+
+    @trigonometry.command()
+    async def cosh(self, ctx, num : Union[int, float]):
+        return await ctx.send(math.cosh(num))
+    
+    @trigonometry.command()
+    async def acos(self, ctx, num : Union[int, float]):
+        return await ctx.send(math.acos(num))
+
+    @trigonometry.command()
+    async def acosh(self, ctx, num : Union[int, float]):
+        return await ctx.send(math.acosh(num))
+    
+    @trigonometry.command()
+    async def tan(self, ctx, num : Union[int, float]):
+        return await ctx.send(math.tan(num))
+
+    @trigonometry.command()
+    async def tanh(self, ctx, num : Union[int, float]):
+        return await ctx.send(math.tanh(num))
+
+    @trigonometry.command()
+    async def atan(self, ctx, num : Union[int, float]):
+        return await ctx.send(math.atan(num))
+    
+    @trigonometry.command()
+    async def atanh(self, ctx, num : Union[int, float]):
+        return await ctx.send(math.atanh(num))
 
 def setup(client):
     client.add_cog(Math(client))
